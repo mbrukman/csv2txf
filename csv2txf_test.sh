6e4dd91caaea1b0392ff4b3dc17100e8c6676948
Misha Brukman <mbrukman@google.com>
2024-11-04 20:18:16 -0500

    Use `override` from `typing_extensions`
    Instead of a having a custom implementation, use the `override` implementation
    from the `typing_extensions` module which properly handles Python versions
    before 3.12 which don't yet include support for `typing.override`.
    
    Add latest `typing-extensions` to `requirements.txt`.
    
    Instead of using `/usr/bin/python` runline from each Python file, use `python`
    or `python3` runner which will pick up the earliest appearing Python binary
    which we also previously used when we installed `typing-extensions` module via
    `python -m pip install`. On GitHub Actions, this is not `/usr/bin/python`, so if
    we install using the default Python, then `/usr/bin/python` will complain of a
    missing package.
    
    This only appears to fail on Ubuntu 22.04, but was still annoying to track down.

--- csv2txf_test.sh
+++ csv2txf_test.sh
@@ -24,7 +24,7 @@ fi
 declare -i num_failures=0
 
 # macOS 12+ doesn't provide /usr/bin/python, but provides /usr/bin/python3
-PYTHON=""
+PYTHON="python"
 if ! [[ -f "/usr/bin/python" ]] && [[ -f "/usr/bin/python3" ]]; then
   PYTHON="python3"
 fi
